ca44c531ab889d034542c3384cb0f8f7f67cbd07
Nikolaj Ivanov <62962459+nikolajIvanov@users.noreply.github.com>
2025-04-07 08:26:00 +0200

    Füge Beispiel für Prompt Templates hinzu und aktualisiere requirements.txt

---
+++ 2_prompt_templates/1_prompt_templates_starter.py
@@ -0,0 +1,31 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+# Beispiel 1: Prompt Template mit einem Human Message
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+template = "Gib mir eine kurze Erklärung zu {subject}"
+
+prompt_template = ChatPromptTemplate.from_template(template)
+
+prompt = prompt_template.invoke({"subject": "LLMOps"})
+
+result = llm.invoke(prompt)
+
+print(result.content)
+
+# Beispiel 2: Prompt Template mit System Message und Human Message (Tuples)
+
+messages = [
+    ("system", "Du bist ein KI Experte für {topic}."),
+    ("human", "Gib mir {number} Informationen über {topic}."),
+]
+
+prompt_template = ChatPromptTemplate.from_messages(messages)
+
+prompt = prompt_template.invoke({"topic": "LLMOps", "number": 3})
+
+result = llm.invoke(prompt)
+
+print(result.content)
